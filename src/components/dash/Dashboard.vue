f584a2830cde92bec3376e995ce06bd9fbcf82b5
nczkevin <z1376871863@163.com>
2016-08-03 21:37:32 +0800

    update

--- src/components/dash/Dashboard.vue
+++ src/components/dash/Dashboard.vue
@@ -5,19 +5,56 @@
     <div class='row'>
       <div class="shu">
         <div class="col-md-9 main-shu" id="gouzao">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">流程构造</h3>
+              <div class="box-tools pull-right">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <!-- <span class="label label-primary">Label</span> -->
+              </div><!-- /.box-tools -->
+            </div><!-- /.box-header -->
+            <div class="box-body">
+              <div class="main-work">
+                
+              </div>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
         </div>
-        <div class="col-md-2 right-shu" id="canshu">
-          <p>canshu</p>
+        <div class="col-md-3 right-shu" id="canshu">
+          <div class="box box-success">
+            <div class="box-header with-border">
+              <h3 class="box-title">参数设置</h3>
+              <div class="box-tools pull-right">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <!-- <span class="label label-primary">Label</span> -->
+              </div><!-- /.box-tools -->
+            </div><!-- /.box-header -->
+            <div class="box-body">
+              <div class="main-param">
+                
+              </div>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
         </div>
       </div>
       <div class="err-shu col-md-12">
-        <p>error</p>
+        <div class="box box-success">
+          <div class="box-header with-border">
+            <h3 class="box-title">错误提示</h3>
+            <div class="box-tools pull-right">
+              <!-- Buttons, labels, and many other things can be placed here! -->
+              <!-- Here is a label for example -->
+              <!-- <span class="label label-primary">Label</span> -->
+            </div><!-- /.box-tools -->
+          </div><!-- /.box-header -->
+          <div class="box-body">
+            <div class="main-err">
+              
+            </div>
+          </div><!-- /.box-body -->
+        </div><!-- /.box -->
       </div>
     </div>
     <!-- /.row -->
@@ -26,46 +63,21 @@
 </template>
 
 <<style>
-.main-shu,.right-shu {
-  border: 1px solid #312213;
+.main-shu {
   height: 500px;
-  margin-right: 10px;
+}
+.right-shu {
+  height: 500px;
+}
+.err-shu {
+  margin-top: 10px;
 }
 </style>
 
 <script>
-import Chart from 'chart.js'
 
 module.exports = {
   ready: function () {
-    var ctx = document.getElementById('canvas').getContext('2d')
-    var data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          fillColor: 'rgba(220,220,220,0.2)',
-          strokeColor: 'rgba(220,220,220,1)',
-          pointColor: 'rgba(220,220,220,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: 'My Second dataset',
-          fillColor: 'rgba(151,187,205,0.2)',
-          strokeColor: 'rgba(151,187,205,1)',
-          pointColor: 'rgba(151,187,205,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(151,187,205,1)',
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    }
-
-    new Chart(ctx).Line(data, {responsive: false})
   }
 }
 </script>
